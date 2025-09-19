7c6c62f6bf35df86230379be7ef600b30924813f
Johnny19Yuan <johnny19yuan@gmail.com>
2025-09-19 09:09:11 -0400

    challenge3.py

--- challenge3.py
+++ challenge3.py
@@ -1,6 +1,12 @@
 bill = input("What was the bill? ")
-
-service = input("How was the service? ")#bad, okay, good ,great
-
+service = input("How was the service? ")#bad,okay,good,great
 if service == "bad":
-    print 
+    print (f"0% tip worth $0")
+elif service == "okay":
+    print (f"5% tip worth ${float(bill)*0.05}")
+elif service == "good":
+    print (f"10% tip worth ${float(bill)*0.1}")
+elif service == "great":
+    print (f"15% tip worth ${float(bill)*0.15}")
+else:
+    print ("invalid answer")
