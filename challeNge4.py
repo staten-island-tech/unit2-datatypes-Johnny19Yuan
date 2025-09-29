4e78204b0dc4945e20bb012c0fe59062b5ba7989
Johnny19Yuan <johnny19yuan@gmail.com>
2025-09-29 09:24:07 -0400

    update challengfg4
    ye

--- challeNge4.py
+++ challeNge4.py
@@ -1,8 +1,14 @@
 #Create a function that accepts an input and determines all factors of the number.
 number = input("factor the Number: ")
 
-for i in range(x):
-    x = (int(number) % 2)
+factors = []
 
-if x == 0:
-    print("testing")
+divide = (int(number) % 2)
+if divide == 0:
+    factors.append("2")
+    factors.append(f"{divide}")
+else:
+
+print("The factors are:")
+for numbers in factors:
+    print(numbers)
